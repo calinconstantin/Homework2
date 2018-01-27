318ddf361f5b27f2a189c226c895b9e14ceab206
Calin Constantin Popa <calinpopaconstantin@gmail.com>
2018-01-27 09:47:13 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -6,9 +6,9 @@ public class Node
 	public int distanceX =0;
 	public int distanceY =0;	
 	public String Terrain = " ";	
-	public ArrayList<Node> neighbours;
+	public ArrayList<Node> neighbors;
 	public Node ()
 	{
-		neighbours = new ArrayList <Node>();
+		neighbors = new ArrayList <Node>();
 	}	
 }
